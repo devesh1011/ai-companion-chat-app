d0f007bcec90fcd716af0dbdf7ea671eebf7bc6f
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2025-12-03 18:05:00 +0530

    add login function for authentication service

---
+++ src/gateway/auth_svc/access.py
@@ -0,0 +1,23 @@
+import os
+import requests
+
+
+def login(form_data):
+    try:
+        response = requests.post(
+            f"http://{os.getenv('AUTH_SVC_ADDR')}/token",
+            data={
+                "username": form_data.username,
+                "password": form_data.password,
+            },
+            timeout=3,
+        )
+    except requests.exceptions.RequestException as e:
+        return None, (f"auth service unavailable: {str(e)}", 503)
+
+    if response.status_code == 200:
+        # This returns the Token schema: {"access_token": "...", "token_type": "bearer"}
+        return response.json(), None
+    else:
+        # Return the actual error message from the Auth service
+        return None, (response.text, response.status_code)
