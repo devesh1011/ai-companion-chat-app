fe78d6622d7fdd097bff1aa19aacfbfb69ab073c
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2025-12-22 22:35:34 +0530

    refactor reply function to integrate langchain ChatGoogleGenerativeAI

--- src/ai-consumer/character_response/reply.py
+++ src/ai-consumer/character_response/reply.py
@@ -1,12 +1,45 @@
-from typing import Any
-from google import genai
-from google.genai import types
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from langchain.messages import HumanMessage, AIMessage, SystemMessage
+from typing import List
 
-# client = genai.Client(api_key="")
+model = ChatGoogleGenerativeAI(
+    api_key="AIzaSyB7gCgdy3iq1wB40uxPNliyZmDIi7PL9Bs",
+    model="gemini-3-flash-preview",
+    temperature=1.0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
 
 
-def reply(message: str, sytem_prompt: str, conversation_history):
-    # response = client.models.generate_content(model="", contents="")
-    print(conversation_history)
-    print(message)
-    return "hello"
+async def reply(latest_msg: dict, system_prompt: str, conversation_history: List[dict]):
+    system_message = SystemMessage(
+        content=f"{system_prompt}. Generate respoonse by taking the following conversation messages in account"
+    )
+
+    conversation_messages = []
+    for i, message in enumerate(conversation_history):
+        if i != len(conversation_history) - 1:  # Skip last message
+            role = message.get("role", "user").lower()
+            content = message.get("content", "")
+            if role == "user":
+                conversation_messages.append(HumanMessage(content=content))
+            elif role == "ai":
+                conversation_messages.append(AIMessage(content=content))
+
+    # Build final messages list
+    messages = (
+        [system_message] + conversation_messages + [HumanMessage(latest_msg["content"])]
+    )
+
+    response = await model.ainvoke(messages)
+
+    if hasattr(response, "content"):
+        content = response.content
+        if isinstance(content, list) and len(content) > 0:
+            if isinstance(content[0], dict) and "text" in content[0]:
+                content = content[0]["text"]
+        return {"role": "ai", "content": str(content)}
+    else:
+        return {"role": "ai", "content": str(response)}
